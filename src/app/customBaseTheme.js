b973e4976724a1b36688135e5b4723ce1f1c3e91
Larry Maccherone <Larry@Maccherone.com>
2016-02-08 18:52:48 -0800

    More custom theme tweaks

--- src/app/customBaseTheme.js
+++ src/app/customBaseTheme.js
@@ -7,18 +7,16 @@ export default {
   palette: {
     primary1Color: '#DE1E54',  // App Bar background, Tab bar background, completed (left) part of slider, checkboxes/radio selected, toggle-on color
     primary2Color: Colors.cyan700,  // Background for heading on date picker, selected date background
-    primary3Color: Colors.grey600,  // Background of toggles
+    primary3Color: Colors.grey800,  // Background of toggles
     accent1Color: Colors.grey400,  // Tab active indicator, Primary button background, Snackbar action text
-    //accent2Color: '#DE1E54',  // Toggle-off color
     accent2Color: Colors.black,  // Toggle-off color
-    accent3Color: ColorManipulator.fade(Colors.black, 0.5),  // Hover color for right side of slider
-    textColor: ColorManipulator.fade(Colors.fullWhite, 0.8),  // Most text, dropdowns, etc.
-    alternateTextColor: Colors.black,  // Text in AppBar, Text for selected Tab, background for default button, text for primary and secondary buttons
+    accent3Color: Colors.grey300,  // Hover color for right side of slider
+    textColor: Colors.grey100,  // Most text, dropdowns, etc.
+    alternateTextColor: Colors.grey800,  // Text in AppBar, Text for selected Tab, background for default button, text for primary and secondary buttons
     canvasColor: '#0B253D',
-    borderColor: ColorManipulator.fade(Colors.fullWhite, 0.5),  // Underline for text fields (unfocused), round button on disabled toggles, Dividers
-    disabledColor: ColorManipulator.fade(Colors.fullWhite, 0.3),  // Hint text, text on disabled things
+    borderColor: Colors.grey200,  // Underline for text fields (unfocused), round button on disabled toggles, Dividers
+    disabledColor: Colors.grey600,  // Hint text, text on disabled things
     pickerHeaderColor: Colors.green700,
-    //clockCircleColor: ColorManipulator.fade(Colors.fullWhite, 0.12),
     clockCircleColor: Colors.green700,
   },
 };
